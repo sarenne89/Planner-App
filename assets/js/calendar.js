bbad443aa93acdabff786ce997601ceea82dd449
Matt Dunmore <mdunmore89@gmail.com>
2024-01-19 19:49:47 +0000

    planner can now detect the time and add classes accordingly

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,31 +1,52 @@
+var today = dayjs();
 const calendarTimesArray = [
-  "9AM",
-  "10AM",
-  "11AM",
-  "12PM",
-  "1PM",
-  "2PM",
-  "3PM",
-  "4PM",
+  //please excuse this minor hack!
+  "0" + dayjs().hour(9).format("H A"),
+  dayjs().hour(10).format("H A"),
+  dayjs().hour(11).format("H A"),
+  dayjs().hour(12).format("H A"),
+  dayjs().hour(13).format("H A"),
+  dayjs().hour(14).format("H A"),
+  dayjs().hour(15).format("H A"),
+  dayjs().hour(16).format("H A"),
+  dayjs().hour(17).format("H A"),
 ];
 const timeSlots = $("#timeSlots");
 
-for (let i = 0; i < calendarTimesArray.length; i++) {
-  var timeSlot = $("<div>");
-  timeSlot.addClass("row");
-  timeSlots.append(timeSlot);
-  var hour = $("<div>");
-  hour.addClass("hour col-2 d-flex align-items-center justify-content-center");
-  hour.appendTo(timeSlot);
-  hour.text(calendarTimesArray[i]);
-  var description = $("<div>");
-  description.addClass(
-    "description future col-8 d-flex align-items-center justify-content-center"
-  );
-  description.appendTo(timeSlot);
-  var saveButton = $("<div>");
-  saveButton.addClass(
-    "col-2 saveBtn d-flex align-items-center justify-content-center"
-  );
-  saveButton.appendTo(timeSlot);
-}
+$(document).ready(function () {
+  //creates calendar layout
+  for (let i = 0; i < calendarTimesArray.length; i++) {
+    //creates a row for each time slot
+    var timeSlot = $("<div>");
+    timeSlot.addClass("row");
+    timeSlots.append(timeSlot);
+    //creates a div for the time
+    var hour = $("<div>");
+    hour.addClass(
+      "hour col-2 d-flex align-items-center justify-content-center"
+    );
+    hour.appendTo(timeSlot);
+    hour.text(calendarTimesArray[i]);
+    //creates the text box for the task description input
+    var description = $("<div>");
+    description.addClass(
+      "description col-8 d-flex align-items-center justify-content-center"
+    );
+    if (today.format("H A") > calendarTimesArray[i]) {
+      description.addClass("past");
+    } else if (today.format("H A") === calendarTimesArray[i]) {
+      description.addClass("present");
+    }
+    if (today.format("H A") < calendarTimesArray[i]) {
+      description.addClass("future");
+    }
+    description.attr("contenteditable: true");
+    description.appendTo(timeSlot);
+    //creates the save button for each time slot
+    var saveButton = $("<div>");
+    saveButton.addClass(
+      "col-2 saveBtn d-flex align-items-center justify-content-center"
+    );
+    saveButton.appendTo(timeSlot);
+  }
+});
